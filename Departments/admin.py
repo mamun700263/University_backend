3e56114b8e497507d0fd0f318bb13fd69619f87a
mamun700263 <mamun700263@gmail.com>
2024-11-19 17:12:54 +0600

    changed the enve and installed required files

--- Departments/admin.py
+++ Departments/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import Department
+from .models import Batch
 # Register your models here.
 # make sure to add the app in the installed app list
-# admin.site.register(Account)
-# admin.site.register(StudentAccount)
-# admin.site.register(TeacherAccount)
-# admin.site.register(AuthorityAccount)
-# admin.site.register(StaffAccount)
+admin.site.register(Department)
+admin.site.register(Batch)
