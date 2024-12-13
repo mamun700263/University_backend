96d0ba17eb8b998064fdb8b8c287454d5989a915
mamun700263 <mamun700263@gmail.com>
2024-12-13 14:03:28 +0600

    started the project

--- university/models.py
+++ university/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from Departments.models import Department
 # Create your models here.
+
+class University(models.Model):
+
+    name = models.CharField(max_length=50)
+    departments = models.maToManyField("app.Model", verbose_name=(""))
+    
