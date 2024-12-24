01b692f521bb7bd6611d03816d578d53b08b4a86
mamun700263 <mamun700263@gmail.com>
2024-12-24 20:38:07 +0600

    path get post views are set

--- university/views.py
+++ university/views.py
@@ -1,8 +1,44 @@
-
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import University
-from .Serializer import UniversitySerializer
+from .serializers import UniversitySerializer
 
-class UniversityViewSet(viewsets.ModelViewSet):
-    queryset = University.objects.all()
-    serializer_class = UniversitySerializer
+class UniversityAPIView(APIView):
+    def get(self, request):
+        universities = University.objects.all()
+        serializer = UniversitySerializer(universities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        # print("***********************")
+        # print(request.data)
+        serializer = UniversitySerializer(data=request.data)
+        # print(serializer.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UniversityModifyView(APIView):
+    def get(self, request, id, *args, **kwargs):
+        try:
+            # Retrieve the university by id
+            university = University.objects.get(id=id)
+        except University.DoesNotExist:
+            return Response({"error": "University not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        # Serialize the university data
+        serializer = UniversitySerializer(university)
+        return Response(serializer.data)
+    def patch(self, request, id, *args, **kwargs):
+        try:
+            university = University.objects.get(id=id)
+        except University.DoesNotExist:
+            return Response({"error": "University Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        # Validate and update with the serializer
+        serializer = UniversitySerializer(university, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
