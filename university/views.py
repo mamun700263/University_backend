7449fb1b125bda99bec5009bffc9541dd578d251
TrishaPal2003 <your-email@example.com>
2025-05-09 18:50:17 +0600

    chore: applied pep8 to university (#11)

--- university/views.py
+++ university/views.py
@@ -9,25 +9,40 @@ from .serializers import UniversitySerializer
 class UniversityAPIView(APIView):
     def get(self, request):
         universities = University.objects.all()
-        serializer = UniversitySerializer(universities, many=True)
-        return Response(serializer.data)
+        serializer = UniversitySerializer(
+            universities,
+            many=True
+        )
+        return Response(
+            serializer.data
+        )
 
     def post(self, request):
         # print("***********************")
         # print(request.data)
-        serializer = UniversitySerializer(data=request.data)
+        serializer = UniversitySerializer(
+            data=request.data
+        )
         # print(serializer.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class UniversityModifyView(APIView):
     def get(self, request, id, *args, **kwargs):
         try:
             # Retrieve the university by id
-            university = University.objects.get(id=id)
+            university = University.objects.get(
+                id=id
+            )
         except University.DoesNotExist:
             return Response(
                 {"error": "University not found."},
@@ -36,11 +51,15 @@ class UniversityModifyView(APIView):
 
         # Serialize the university data
         serializer = UniversitySerializer(university)
-        return Response(serializer.data)
+        return Response(
+            serializer.dat
+        )
 
     def patch(self, request, id, *args, **kwargs):
         try:
-            university = University.objects.get(id=id)
+            university = University.objects.get(
+                id=id
+            )
         except University.DoesNotExist:
             return Response(
                 {"error": "University Not Found"},
@@ -49,8 +68,17 @@ class UniversityModifyView(APIView):
 
         # Validate and update with the serializer
         serializer = UniversitySerializer(
-            university, data=request.data, partial=True)
+            university,
+            data=request.data,
+            partial=True
+        )
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
