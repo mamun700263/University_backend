bfcda0ede490b8377be7cf1702ccb9b326d591d4
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-07-29 20:27:30 +0600

    feature #72: updated course app

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Course, Enrollment
+from .serializers import CourseSerializer, EnrollmentSerializer
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
